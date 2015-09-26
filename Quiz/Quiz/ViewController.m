521e6aa3c73c096d09ebcfc31186fceeee3d05e2
Leon-Kang <git@github.com:Leon-Kang>
2015-09-26 20:11:11 +0800

    Quiz

---
+++ Quiz/Quiz/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Quiz
+//
+//  Created by 康梁 on 15/9/26.
+//  Copyright © 2015年 kl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+
+@end
+
+@implementation ViewController
+
+- (IBAction)showQuestion:(id)sender
+{
+    
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    
+}
+
+@end
